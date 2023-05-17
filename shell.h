11c08f0493f2712e073e45a06146bda8a26162ef
khayrat18 <hairatua@yahoo.com>
2023-05-17 15:30:48 -0700

    simple shell project

--- shell.h
+++ shell.h
@@ -1,12 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdlib>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
-#include <sy/wait>
+#include <sys/wait.h>
 #include <sys/stat.h>
 
 typedef struct list_s
