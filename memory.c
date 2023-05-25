b54985fdfb19c54fb6a9065a2808b711d8905d4e
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-05-25 20:40:09 +0000

    Fix memory endless loop

--- memory.c
+++ memory.c
@@ -1,11 +1,6 @@
 #include "shell.h"
 
-/**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
- *
- * Return: 1 if freed, otherwise 0.
- */
+
 int bfree(void **ptr)
 {
 	if (ptr && *ptr)
